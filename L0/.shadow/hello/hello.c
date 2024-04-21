7fce1b4bee575f1aad190ffff27660a87d16cd88
tracer-nju <tracer@nju.edu.cn>
2024-04-21 19:01:32 +0800

    Linux Laptop-Zhu 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:01:32 up  2:56,  0 users,  load average: 0.33, 0.12, 0.09

---
+++ L0/.shadow/hello/hello.c
@@ -0,0 +1,11 @@
+//
+// Created by zhuzr on 4/19/24.
+//
+#include <am.h>
+#include <klib.h>
+#include <stdarg.h>
+
+int main(const char *args) {
+    putch('1');
+    return 0;
+}
---
+++ L0/kernel/build/x86_64-qemu/kernel.d
@@ -0,0 +1,8 @@
+/home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/build/x86_64-qemu/kernel.o: \
+ /home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/kernel.c \
+ /home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/../abstract-machine/am/include/am.h \
+ /home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/../abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/../abstract-machine/am/include/amdev.h \
+ /home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/../abstract-machine/am/include/amdev.h \
+ /home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/../abstract-machine/klib/include/klib.h \
+ /home/zhuzr/projects/nju-os/nju-oslab/L0/kernel/../abstract-machine/klib/include/klib-macros.h
