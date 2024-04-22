f4331edaac631873696bda24294b059fa8ecccde
tracer-nju <tracer@nju.edu.cn>
2024-04-22 20:00:03 +0800

    Linux Laptop-Zhu 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:00:03 up  3:04,  0 users,  load average: 2.58, 1.11, 0.55

--- L0/.shadow/kernel/kernel.c
+++ L0/.shadow/kernel/kernel.c
@@ -3587,7 +3587,7 @@ void splash() {
     for (int x = 0; x * SIDE <= w; x ++) {
         for (int y = 0; y * SIDE <= h; y++) {
             if ((x & 1) ^ (y & 1)) {
-                draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, __0x3f_jpg[x]);     // white
+                draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, __0x3f_jpg[x * w + y]);     // white
             }
         }
     }
