55dedf75f21ce9ae97250aa229d68271e9636afb
tracer-nju <tracer@nju.edu.cn>
2024-04-22 21:35:15 +0800

    Linux Laptop-Zhu 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:35:15 up  4:39,  0 users,  load average: 0.95, 0.80, 0.82

--- L0/.shadow/kernel/kernel.c
+++ L0/.shadow/kernel/kernel.c
@@ -51,8 +51,8 @@ void splash() {
     h = info.height;
     // w - screen width
     // h - screen height
-    float w_rate = 556 / w / SIDE;
-    float h_rate = 556 / h / SIDE;
+    float w_rate = (float) (556 / SIDE) / w;
+    float h_rate = (float) (556 / SIDE) / h;
 
     for (int x = 0; x * SIDE <= w; x ++) {
         for (int y = 0; y * SIDE <= h; y++) {
