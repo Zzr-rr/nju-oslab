baaee48b52871bf7ea7e4a4a347a70ead3c3e4ae
tracer-nju <tracer@nju.edu.cn>
2024-04-22 18:17:04 +0800

    Linux Laptop-Zhu 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:17:04 up  1:21,  0 users,  load average: 0.52, 0.16, 0.10

--- L0/.shadow/kernel/kernel.c
+++ L0/.shadow/kernel/kernel.c
@@ -43,18 +43,18 @@ static void draw_tile(int x, int y, int w, int h, uint32_t color) {
 }
 
 void splash() {
-  AM_GPU_CONFIG_T info = {0};
-  ioe_read(AM_GPU_CONFIG, &info);
-  w = info.width;
-  h = info.height;
-
-  for (int x = 0; x * SIDE <= w; x ++) {
-    for (int y = 0; y * SIDE <= h; y++) {
-      if ((x & 1) ^ (y & 1)) {
-        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, x * 100 + y); // white
-      }
+    AM_GPU_CONFIG_T info = {0};
+    ioe_read(AM_GPU_CONFIG, &info);
+    
+    w = info.width;
+    h = info.height;
+    for (int x = 0; x * SIDE <= w; x ++) {
+        for (int y = 0; y * SIDE <= h; y++) {
+            if ((x & 1) ^ (y & 1)) {
+                draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, x * 100 + y); // white
+            }
+        }
     }
-  }
 }
 
 // Operating system is a C program!
