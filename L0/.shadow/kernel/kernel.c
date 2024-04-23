e0270382f9c39f6e53e150b06545421d7beb67d4
tracer-nju <tracer@nju.edu.cn>
2024-04-23 10:37:43 +0800

    Linux Laptop-Zhu 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:37:43 up 9 min,  0 users,  load average: 0.87, 0.70, 0.36

--- L0/.shadow/kernel/kernel.c
+++ L0/.shadow/kernel/kernel.c
@@ -4,43 +4,43 @@
 #include <klib-macros.h>
 #include "img.h"
 
-#define SIDE 2
+#define SIDE 1
 
 static int w, h;  // Screen size
 
 #define KEYNAME(key) \
   [AM_KEY_##key] = #key,
-static const char *key_names[] = { AM_KEYS(KEYNAME) };
+static const char *key_names[] = {AM_KEYS(KEYNAME)};
 
 static inline void puts(const char *s) {
-  for (; *s; s++) putch(*s);
+    for (; *s; s++) putch(*s);
 }
 
 void print_key() {
-  AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
-  ioe_read(AM_INPUT_KEYBRD, &event);
-  if (event.keycode != AM_KEY_NONE && event.keydown) {
-    puts("Key pressed: ");
-    puts(key_names[event.keycode]);
-    puts("\n");
+    AM_INPUT_KEYBRD_T event = {.keycode = AM_KEY_NONE};
+    ioe_read(AM_INPUT_KEYBRD, &event);
+    if (event.keycode != AM_KEY_NONE && event.keydown) {
+        puts("Key pressed: ");
+        puts(key_names[event.keycode]);
+        puts("\n");
 
-    // Keyboard ESCAPE will interrupt the system.
-    if(event.keycode == AM_KEY_ESCAPE){
-        halt(1);
+        // Keyboard ESCAPE will interrupt the system.
+        if (event.keycode == AM_KEY_ESCAPE) {
+            halt(1);
+        }
     }
-  }
 }
 
 static void draw_tile(int x, int y, int w, int h, uint32_t color) {
-  uint32_t pixels[w * h];   // WARNING: large stack-allocated memory
-  AM_GPU_FBDRAW_T event = {
-    .x = x, .y = y, .w = w, .h = h, .sync = 1,
-    .pixels = pixels,
-  };
-  for (int i = 0; i < w * h; i++) {
-    pixels[i] = color;
-  }
-  ioe_write(AM_GPU_FBDRAW, &event);
+    uint32_t pixels[w * h];   // WARNING: large stack-allocated memory
+    AM_GPU_FBDRAW_T event = {
+            .x = x, .y = y, .w = w, .h = h, .sync = 1,
+            .pixels = pixels,
+    };
+    for (int i = 0; i < w * h; i++) {
+        pixels[i] = color;
+    }
+    ioe_write(AM_GPU_FBDRAW, &event);
 }
 
 void splash() {
@@ -54,11 +54,12 @@ void splash() {
     int w_pixel = w / SIDE;
     int h_pixel = h / SIDE;
 
-    for (int x = 0; x * SIDE <= w; x ++) {
+    for (int x = 0; x * SIDE <= w; x++) {
         for (int y = 0; y * SIDE <= h; y++) {
-            int color_index = (int)((x * 556 * 556 / w_pixel)) + (int)(y * 556 / h_pixel);
-            if (color_index < 556 * 556) {   // assuming __0x3f_jpg has 556*556 elements
-                draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, __0x3f_jpg[color_index]);
+            int mapping_x = (img_width / w_pixel) * x;
+            int mapping_y = (img_height / h_pixel) * y;
+            if (mapping_x * img_width + mapping_y < img_width * img_height) {   // assuming __0x3f_jpg has 556*556 elements
+                draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, __0x3f_jpg[mapping_x * img_width + mapping_y]);
             }
         }
     }
@@ -66,17 +67,17 @@ void splash() {
 
 // Operating system is a C program!
 int main(const char *args) {
-  ioe_init();
+    ioe_init();
 
-  puts("mainargs = \"");
-  puts(args);  // make run mainargs=xxx
-  puts("\"\n");
+    puts("mainargs = \"");
+    puts(args);  // make run mainargs=xxx
+    puts("\"\n");
 
-  splash();
+    splash();
 
-  puts("Press any key to see its key code...\n");
-  while (1) {
-    print_key();
-  }
-  return 0;
+    puts("Press any key to see its key code...\n");
+    while (1) {
+        print_key();
+    }
+    return 0;
 }
